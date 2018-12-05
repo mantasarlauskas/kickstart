3d88fea8aa72d85475baf7449d94869792fb1efa
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2018-12-05 22:50:33 +0200

    Added MoneyFormatter test class

---
+++ tests/MoneyFormatterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MoneyFormatterTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
