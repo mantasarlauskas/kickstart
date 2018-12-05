5b17c27206963b9f189eb801ff324badae9c0f82
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2018-12-05 23:06:28 +0200

    Added tests to NumberFormatter test class

--- tests/NumberFormatterTest.php
+++ tests/NumberFormatterTest.php
@@ -2,12 +2,49 @@
 
 namespace App\Tests;
 
+use App\Services\NumberFormatter;
 use PHPUnit\Framework\TestCase;
 
 class NumberFormatterTest extends TestCase
 {
-    public function testSomething()
+
+    /**
+     * @var NumberFormatter $numberFormatter
+     */
+    private $numberFormatter;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        $this->numberFormatter = new NumberFormatter();
+    }
+
+    public function testMillionFormatting()
+    {
+        $this->assertEquals('2.84M', $this->numberFormatter->format(2835779));
+        $this->assertEquals('1.00M', $this->numberFormatter->format(999500));
+    }
+
+    public function testHundredsThousands()
+    {
+        $this->assertEquals('535K', $this->numberFormatter->format(535216));
+        $this->assertEquals('100K', $this->numberFormatter->format(99950));
+    }
+
+    public function testThousands()
+    {
+        $this->assertEquals('27 534', $this->numberFormatter->format(27533.78));
+        $this->assertEquals('1 000', $this->numberFormatter->format(999.9999));
+    }
+
+    public function testDefault()
+    {
+        $this->assertEquals('533.10', $this->numberFormatter->format( 533.1));
+        $this->assertEquals('66.67', $this->numberFormatter->format(66.6666));
+        $this->assertEquals('12.00', $this->numberFormatter->format(12));
+    }
+    
+    public function testNegative()
+    {
+        $this->assertEquals('-124K', $this->numberFormatter->format(-123654.89));
     }
 }
