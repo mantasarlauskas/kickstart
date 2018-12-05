1ad16fc0a6381e7f5bc9a386d6a5c547b093bcdd
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2018-12-06 00:43:38 +0200

    Added tests to MoneyFormatter test class

--- tests/MoneyFormatterTest.php
+++ tests/MoneyFormatterTest.php
@@ -2,12 +2,49 @@
 
 namespace App\Tests;
 
+use App\Services\MoneyFormatter;
+use App\Services\NumberFormatter;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class MoneyFormatterTest extends TestCase
 {
-    public function testSomething()
+    /**
+     * @var MoneyFormatter $moneyFormatter
+     */
+    private $moneyFormatter;
+
+    /** @var NumberFormatter|MockObject $numberFormatter */
+    private $numberFormatter;
+
+    /**
+     * @param int $index
+     * @param float $argument
+     * @param string $returnValue
+     */
+    private function addCase(int $index, float $argument, string $returnValue)
     {
-        $this->assertTrue(true);
+        $this->numberFormatter->expects($this->at($index))
+            ->method('format')
+            ->with($argument)
+            ->will($this->returnValue($returnValue));
+    }
+
+    public function testFormatCurrency()
+    {
+        $this->numberFormatter = $this->createMock(NumberFormatter::class);
+
+        $this->addCase(0, 2835779, '2.84M');
+        $this->addCase(1, 211.99, '211.99');
+        $this->addCase(2, 2835779, '2.84M');
+        $this->addCase(3, 211.99, '211.99');
+
+        $this->moneyFormatter = new MoneyFormatter($this->numberFormatter);
+
+        $this->assertEquals('2.84M €', $this->moneyFormatter->formatEur(2835779));
+        $this->assertEquals('211.99 €', $this->moneyFormatter->formatEur(211.99));
+
+        $this->assertEquals('$2.84M', $this->moneyFormatter->formatUsd(2835779));
+        $this->assertEquals('$211.99', $this->moneyFormatter->formatUsd(211.99));
     }
 }
